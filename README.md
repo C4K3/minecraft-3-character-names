b1807492531d64432c002524536e17f7e3c3958c
Sebastian Blunt <sebastian@c4k3.net>
2016-02-21 11:28:36 +0100

    Add clojure script

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-All available 3 character Minecraft names
+All available 3 character/letter Minecraft names
 =====
 
-This is a small bash script that queries the Minecraft API to test for all available 3 character names.
+This is a small clojure script that queries the Minecraft API to test for all available 3 character names.
 
 The found playernames are in *available-now.txt* (currently available names) and *soon-available.txt* (not currently in use but still under the 37 day protection.) Do note that these lists are NOT automatically updated, but instead show which names were available when the script was last run. If you've got a newer copy of these lists, feel free to submit a pull request.
+
+The old get-names.sh script is included as well, it should still work but is significantly slower (requiring over 100k requests to the Minecraft API compared to about 20k for get-names.clj)
---
+++ get-names.clj
@@ -0,0 +1,102 @@
+(require '[clojure.string :as str]
+         '[clojure.set :as sets])
+(import java.net.URL)
+
+(def characters ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "_" "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"])
+(def possible-names (for [a characters b characters c characters] (str a b c)))
+
+(defn send-usernames-request [request]
+  "Do a Playernames -> UUIDs request"
+  (loop []
+    (let
+      [conn (.. (new URL "https://api.mojang.com/profiles/minecraft") openConnection)]
+      (do
+        (. conn setRequestMethod "POST")
+        (. conn setRequestProperty "Content-Type" "application/json")
+        (. conn setDoOutput true)
+        (spit (. conn getOutputStream) request))
+      (if (not= 200 (int (. conn getResponseCode)))
+        (do
+          (println "Did not get a 200 response, sleeping for 15 seconds")
+          (Thread/sleep 15000)
+          (recur))
+        ;(slurp (. conn getInputStream))
+        (set
+          (map
+            (fn [x] (second (re-find #"name\":\"(\S+?)\"" (str/lower-case x))))
+            (str/split
+              (let [resp (slurp (. conn getInputStream))]
+                (println "Response:" resp)
+                resp)
+              #"\},\{"))))
+      )))
+
+(defn create-request-payload [names]
+  "Create the json payload accepted by the API given a vector of names"
+  (str "["
+       (reduce
+         (fn [a b] (str a ",\"" b "\""))
+         (str "\"" (first names) "\"")
+         (rest names)
+         )
+       "]"))
+
+(defn get-all []
+  "Get all available-now or soon-available names"
+  (loop [available #{} remaining-names possible-names]
+    (if (empty? remaining-names)
+      available
+      (let [names (take 100 remaining-names)]
+        (let [response (send-usernames-request (create-request-payload names))
+              avail (sets/difference (set names) response)]
+          (println "Trying for names:" names)
+          (recur (sets/union available avail) (nthrest remaining-names 100)))))))
+
+;(println (get-all))
+(defn send-at-time-request [name]
+  "Send a Username -> UUID at time request, returns true if name was in use 37 days ago"
+  (loop []
+    (let
+      [past-time (- (quot (System/currentTimeMillis) 1000) (* 60 60 24 37))
+       url (str "https://api.mojang.com/users/profiles/minecraft/" name "?at=" past-time)
+       conn (.. (new URL url) openConnection)
+       response (int (. conn getResponseCode))]
+      (cond
+        (= response 200) (do
+                           (println "Setting" name "to soon-available")
+                           true)
+        (= response 204) (do
+                           (println "Setting" name "to available-now")
+                           false)
+        :else (do
+                (println "Got an error (presumably Too Many Requests,) sleeping for 15 seconds")
+                (Thread/sleep 15000)
+                (recur))))))
+
+(println (get-all))
+(println "AFTER GET ALL")
+
+(defn check-all []
+  "Check all names, returns two sets of available-now and soon-available"
+  (loop [remaining-names (into [] (reverse (sort (get-all))))
+         available-now []
+         soon-available []]
+    (if (empty? remaining-names)
+      [available-now soon-available]
+      (if (send-at-time-request (peek remaining-names))
+        (recur (pop remaining-names)
+               available-now
+               (conj soon-available (peek remaining-names)))
+        (recur (pop remaining-names)
+               (conj available-now (peek remaining-names))
+               soon-available)))))
+
+(let [res (check-all)
+      available-now (first res)
+      soon-available (second res)]
+  (with-open [soon-writer (clojure.java.io/writer "./soon-available.txt")
+              now-writer (clojure.java.io/writer "./available-now.txt")]
+    (doall (for [name (sort soon-available)] (. soon-writer write (str name "\n"))))
+    (doall (for [name (sort available-now)] (. now-writer write (str name "\n"))))
+    ))
+
